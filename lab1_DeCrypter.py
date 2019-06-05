cd3cefedbe6f5cbb6c948318c35f6d4e3c504be5
Howard M Chen <31971182+hchen88@users.noreply.github.com>
2019-06-04 22:14:59 -0700

    Add files via upload

--- lab1_DeCrypter.py
+++ lab1_DeCrypter.py
@@ -1,49 +1,56 @@
-# -*- coding: utf-8 -*-
-"""
-@author Howard Chen
 
 """
+CECS 378
+Lab 1 Decrypter
+June 4, 2019
+@author: Howard Chen
+Professor Giacalone
+Purpose: This program decrypts using simple-substitution
 
-#encrypted string
+"""
 
-#key in the form of a list
-keyList = []
+#key in the form of a string
 keyString = ""
 
 
 """
-encrypt function definition 
-parameters : msg- msg to be encrypted as a string
+decrypt function definition 
+parameters : msg- msg to be decrypted as a string
             keyString - key as a string 
 return : none
 """
 def decrypt(msg, keyString):
-    decryptedString = ""
+    keyList = [] #key in list variable
+    decryptedString = "" #decrpyted empty string
+    #list of alphbets makes it easier to convert letters
     alphaList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
                     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
-                    "x", "y", "z"]
-    for letter in keyString:
-        keyList.append(letter)
+                    "x", "y", "z"] 
+    for letter in keyString: #loops through keystring one letter at a time
+        keyList.append(letter) #appends to empty keylist
         
-    print("Key: ")
+    print("Key: ") #prints keylist
     print(keyList)
     
-    for letter in msg:
-        letter = letter.lower() #makes any uppercase letter lower
-        if(letter.isalpha()):
-            decryptedString += alphaList[keyList.index(letter)]
-        else:
-            decryptedString += letter
+    for char in msg: #iterates through msg to decrypt one letter at a time
+        char = char.lower() #makes any uppercase letter lower
+        if(char.isalpha()): #checks if chartacter is a letter.
+            #converts to correct letter
+            decryptedString += alphaList[keyList.index(char)] 
+        else: 
+            decryptedString += char #concantenates any spaces or punctuations 
     print("Decrpyted Msg: " + decryptedString)
     
 #prompt user to enter phrase to encrypt    
 msg = input("Please enter in a phrase to decrypt: ")
-
+#loops until keyString is a valid 26 characters
 while(len(keyString) != 26 ):
+    #prompts for key
     keyString = input("Please enter in a key (26 char): ")
+    #checks for length
     if(len(keyString) != 26):
         print("Invalid Key is not 26 char in length! Retry")
-
+#decrypt function call
 decrypt(msg, keyString)
 
 
--- lab1_Encrypter.py
+++ lab1_Encrypter.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
+
 """
-@author Howard Chen
+CECS 378
+Lab 1 Encrypter
+June 4, 2019
+@author: Howard Chen
+Professor Giacalone
+Purpose: This program encrypts using simple-substitution
 
 """
 
@@ -18,27 +23,28 @@ return : none
 """
 def encrypt(msg, keyString):
     encryptedString = ""
-    for letter in keyString:
+    for letter in keyString: #converts keyString to keyList
         keyList.append(letter)
         
-    print("Key: ")
+    print("Key: ") #outputs keylist
     print(keyList)
     
-    for letter in msg:
-        letter = letter.lower() #makes any uppercase letter lower
-        if(letter.isalpha()):
-            encryptedString += keyList[ord(letter) - 97] 
+    for char in msg: #iterates through message one char at a time
+        char = char.lower() #makes any uppercase letter lower
+        if(char.isalpha()):
+            encryptedString += keyList[ord(char) - 97]#encrypts letter with key
         else:
-            encryptedString += letter
+            encryptedString += char #appends any noncharacters without encrypt
     print("Encrpyted Msg: " + encryptedString)
     
 #prompt user to enter phrase to encrypt    
 msg = input("Please enter in a phrase to encrypt: ")
 
-while(len(keyString) != 26 ):
-    keyString = input("Please enter in a key (26 char): ")
-    if(len(keyString) != 26):
-        print("Invalid Key is not 26 char in length! Retry")
+#loops until user enters valid 26 char key
+while(len(keyString) != 26 ): 
+    keyString = input("Please enter in a key (26 char): ") #prompts user for key
+    if(len(keyString) != 26): #checks for 26 char length for key
+        print("Invalid Key is not 26 char in length! Retry") #prints error msg
 
 #calls function to encrypt 
 encrypt(msg, keyString)
